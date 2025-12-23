4c38c5f7f1d6c0019e0030edff2bd14d435a1bc6
sailakshmi-0987 <gajulapallisailakshmi@gmail.com>
2025-12-23 11:55:15 +0530

    changes in index.js according to client dashboard

--- server/index.js
+++ server/index.js
@@ -2,17 +2,17 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 
-
 const MIN_CLIENTS = 2;
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: "*" }
-});
+const io = new Server(server, { cors: { origin: "*" } });
 
-let clients = {};
+let connectedClients = new Set();
 let updates = [];
+let round = 0;
+let accuracyHistory = [];
+
 function weightedAdd(a, b, weight) {
   if (Array.isArray(a)) {
     return a.map((v, i) => weightedAdd(v, b[i], weight));
@@ -28,26 +28,19 @@ function scaleArray(arr, scale) {
 }
 
 function fedAvg(updates) {
-  const totalSamples = updates.reduce(
-    (sum, u) => sum + u.samples,
-    0
-  );
-
+  const totalSamples = updates.reduce((s, u) => s + u.samples, 0);
   const numLayers = updates[0].weights.length;
   const aggregated = [];
 
   for (let l = 0; l < numLayers; l++) {
     let layerSum = null;
 
-    updates.forEach((u) => {
+    updates.forEach(u => {
       const weight = u.samples / totalSamples;
       const layerData = u.weights[l].data;
-
-      if (!layerSum) {
-        layerSum = scaleArray(layerData, weight);
-      } else {
-        layerSum = weightedAdd(layerSum, layerData, weight);
-      }
+      layerSum = layerSum
+        ? weightedAdd(layerSum, layerData, weight)
+        : scaleArray(layerData, weight);
     });
 
     aggregated.push({
@@ -59,37 +52,47 @@ function fedAvg(updates) {
   return aggregated;
 }
 
-io.on("connection", (socket) => {
-  console.log("Client connected:", socket.id);
+io.on("connection", socket => {
+  connectedClients.add(socket.id);
 
-  socket.on("join", (clientId) => {
-    clients[clientId] = socket.id;
+  // send current dashboard state to NEW client
+  socket.emit("dashboard-update", {
+    round,
+    clients: connectedClients.size,
+    accuracyHistory,
   });
 
-  socket.on("model-update", (data) => {
-  updates.push(data);
+  socket.on("model-update", data => {
+    updates.push(data);
 
-  console.log("Received update count:", updates.length);
+    if (updates.length >= MIN_CLIENTS) {
+      round++;
+      const globalWeights = fedAvg(updates);
+      accuracyHistory.push(data.accuracy);
 
-  if (updates.length >= MIN_CLIENTS) {
-    console.log("Running FedAvg...");
-    const globalWeights = fedAvg(updates);
+      io.emit("global-model", { weights: globalWeights, round });
 
-    io.emit("global-model", {
-      weights: globalWeights,
-      round: 1,
-    });
+      io.emit("dashboard-update", {
+        round,
+        clients: connectedClients.size,
+        accuracyHistory,
+      });
 
-    updates = [];
-  }
-});
-
+      updates = [];
+    }
+  });
 
   socket.on("disconnect", () => {
-    console.log("Client disconnected");
+    connectedClients.delete(socket.id);
+
+    io.emit("dashboard-update", {
+      round,
+      clients: connectedClients.size,
+      accuracyHistory,
+    });
   });
 });
 
-server.listen(5000, () => {
-  console.log("Federated server running on port 5000");
-});
+server.listen(5000, () =>
+  console.log("Federated server running on port 5000")
+);
